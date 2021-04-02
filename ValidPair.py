ed84e0424b34385bf05e86e002962471494abe20
anuragc10 <63997049+anuragc10@users.noreply.github.com>
2021-04-02 17:04:04 +0530

    Create ValidPair.py

---
+++ ValidPair.py
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    if(a==b or a==c or b==c){
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No";
+    }
+}
