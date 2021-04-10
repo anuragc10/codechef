858dada70535565332e0535cde046cd9bce61f3f
anuragc10 <63997049+anuragc10@users.noreply.github.com>
2021-04-10 08:45:55 +0530

    Create worthy matrix.cpp

---
+++ worthy matrix.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<bits/stdc++.h>
+typedef long long int ll;
+using namespace std;
+
+ll minimum(ll a,ll b)
+{
+    if(a<b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif // ONLINE_JUDGE
+
+    ll t;
+    cin>>t;
+    while (t--!=0){
+        ll n,m,k;
+        cin>>n>>m>>k;
+        double mat[n+1][m+1];
+        for( ll i=0;i<=n;i++)
+        {
+            for(ll j=0;j<=m;j++){
+                if(i==0||j==0)
+                {
+                    mat[i][j]=0;
+                }
+                else{
+                    cin>>mat[i][j];
+                }
+            }
+        }
+        for(ll i=0;i<=n;i++)
+        {
+            double pre=0;
+            for(ll j=0;j<=m;j++){
+                mat[i][j]=mat[i][j]+pre;
+                pre=mat[i][j];
+            }
+        }
+        for(ll j=0;j<=m;j++)
+        {
+            double p=0;
+            for(ll i=0;i<=n;i++)
+            {
+                mat[i][j]=mat[i][j]+p;
+                p=mat[i][j];
+            }
+        }
+        ll z=minimum(n,m);
+        ll ans =0;
+        for(ll len=1;len<=z;len++)
+        {
+            for(ll i=len;i<=n;i++)
+            {
+                for(ll j=len;j<=m;j++)
+                {
+                    if((mat[i][j]+mat[i-len][j-len]-mat[i][j-len]-mat[i-len][j])/(len*len)>=k)
+                    {
+                        ans++;
+                    }
+                }
+            }
+        }
+        cout<<ans<<endl;
+    }
+}
