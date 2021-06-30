c31eb39c9996332aae7de536e1ea81b9b584e89d
Anurag Singh <63997049+anuragc10@users.noreply.github.com>
2021-06-30 22:08:29 +0530

    Create traverse array.py

---
+++ traverse array.py
@@ -0,0 +1,30 @@
+t=int(input())
+for _ in range(t):
+    L,h=map(int,input().split())
+    s=input()
+    l=[]
+    a=[]
+    c=0
+    for i in range(L):
+        if(s[i]=='0' and i!=L-1):
+            c=c+1
+        elif(s[i]=='0' and i==L-1):
+            c=c+1
+            a.append(c)
+        elif(i==L-1 or s[i]=='1'):
+            a.append(c)
+            c=0
+    d=0
+    for j in range(len(a)):
+        p=2*(h-a[j])
+        if(p>=h):
+            j=j+1
+        else:
+            h=p
+        if(h<=0):
+            d=1
+            print("YES")
+            break
+    if(d==0):
+        print("NO")
+ 
